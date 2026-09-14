9a7ad9bfb376ee3eab76c152eb8833b31264e57f
eidma <eidma@Cyber-Shinki>
2026-09-14 11:09:45 -0500

    Inicio del proyecto

---
+++ AdrienDelgadoNicoleCaceresBrandolPerez_P1/src/main/java/com/mycompany/adriendelgadonicolecaceresbrandolperez_p1/Menu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adriendelgadonicolecaceresbrandolperez_p1;
+
+/**
+ *
+ * @author eidma
+ */
+public class Menu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
